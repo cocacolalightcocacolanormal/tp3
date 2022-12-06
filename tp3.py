3b42e9c9d7e214937b884f4d9cfdb6b73315aebf
cocacolalightcocacolanormal <112488678+cocacolalightcocacolanormal@users.noreply.github.com>
2022-12-05 19:33:33 -0500

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -1,39 +1,47 @@
 import random
 
 niveau_vie = 20
-force_adversaire = random.randint(1, 5)
-numero_adversaire = 0
+numero_adversaire = 1
 nombre_victoires = 0
 nombre_defaites = 0
-
+dice = 0
 boucle_jeu = True
 while boucle_jeu:
+   force_adversaire = random.randint(1, 6)
    print("Vous tombez face à face avec un adversaire de difficulté", force_adversaire, )
+   print("Niveau de vie:", niveau_vie, )
    choix = int(input("Que voulez-vous faire?\n"
                      "1- Combattre cet adversaire \n"
                      "2- Contourner cet adversaire et aller ouvrir une autre porte \n"
                      "3- Afficher les règles du jeu \n" "4- Quitter la partie \n"))
-
    if choix == 1:
        dice = random.randint(1, 6)
-       numero_adversaire = numero_adversaire + 1
-       print("Adversaire:", numero_adversaire, "\n"
+       if dice <= force_adversaire:
+           print("Vous avez perdu")
+           niveau_vie = niveau_vie - force_adversaire
+           nombre_defaites = nombre_defaites + 1
+           print("Adversaire:", numero_adversaire, "\n"
                                                "Force de l'adversaire", force_adversaire, "\n"
                                                                                           "Niveau de vie de l'usager:",
              niveau_vie, "\n"
                          "Combat", numero_adversaire, "\n"
                                                       ":", nombre_victoires, "vs", nombre_defaites, )
-   if dice <= 4:
-       print("Vous avez perdu")
-       niveau_vie = niveau_vie - 4
-       nombre_defaites = nombre_defaites + 1
-   if dice >= 5:
-       print("Vous avez gagner")
-       niveau_vie = niveau_vie + 4
-       nombre_victoires = nombre_victoires + 1
+       elif dice > numero_adversaire:
+           print("Vous avez gagner")
+           niveau_vie = niveau_vie + force_adversaire
+           nombre_victoires = nombre_victoires + 1
+           print("Adversaire:", numero_adversaire, "\n"
+                                               "Force de l'adversaire", force_adversaire, "\n"
+                                                                                          "Niveau de vie de l'usager:",
+             niveau_vie, "\n"
+                         "Combat", numero_adversaire, "\n"
+                                                      ":", nombre_victoires, "vs", nombre_defaites, )
+
+
    if choix == 2:
        print("Vous decidez de contourner cet adversaire")
        niveau_vie = niveau_vie - 1
+
 
    if choix == 3:
        print("Les regles de se jeu sont simple; Un personnage circule dans un couloir, \n"
@@ -46,5 +54,11 @@ while boucle_jeu:
        print("Bye bye!")
        boucle_jeu = False
 
-if niveau_vie == 0:
-   print("Vous etes morts.")
+
+   if niveau_vie == 0:
+       print("Vous etes morts.")
+       boucle_jeu = False
+
+   if niveau_vie < 0:
+       print("Vous etes morts.")
+       boucle_jeu = False
